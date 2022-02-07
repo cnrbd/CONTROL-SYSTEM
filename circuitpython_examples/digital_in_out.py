b57629d38a1315529d2769fbce9e7bf21ca48311
cnrbd <93989037+cnrbd@users.noreply.github.com>
2022-02-07 10:58:36 +0700

    Update digital_in_out.py

--- circuitpython_examples/digital_in_out.py
+++ circuitpython_examples/digital_in_out.py
@@ -5,13 +5,17 @@ from digitalio import DigitalInOut, Direction, Pull
 
 # LED setup.
 led = DigitalInOut(board.LED)
+#set led as a variable
 
 led.direction = Direction.OUTPUT
+#set led.direction as the output
 
 # Switch setup
 switch = DigitalInOut(board.D2)
 switch.direction = Direction.INPUT
 switch.pull = Pull.UP
+#makes switch an input and assign Pull.up to make sure the switch stays on
+
 
 while True:
     # We could also do "led.value = not switch.value"!
